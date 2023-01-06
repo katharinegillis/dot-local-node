3cd434507276d333c95aef4c7c0b723cd294c494
gillis.katharine@gmail.com <gillis.katharine@gmail.com>
2023-01-05 22:52:06 -0700

    chore: update to latest lts

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Get the binaries package
 cd /tmp || exit
-wget https://nodejs.org/dist/v16.17.0/node-v16.17.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v18.13.0/node-v18.13.0-linux-x64.tar.xz
 
 # Install the binaries
 sudo mkdir -p /usr/local/lib/nodejs
-sudo tar -xJvf node-v16.17.0-linux-x64.tar.xz -C /usr/local/lib/nodejs
+sudo tar -xJvf node-v18.13.0-linux-x64.tar.xz -C /usr/local/lib/nodejs
 
 # Delete the binaries package
-rm -rf /tmp/node-v16.17.0-linux-x64.tar.xz
+rm -rf /tmp/node-v18.13.0-linux-x64.tar.xz
